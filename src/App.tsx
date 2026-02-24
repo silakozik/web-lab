666937b8215443676807e6288489beefb9f6f0d2
silakozik <silakozik2005@gmail.com>
2026-02-24 16:17:22 +0300

    added : app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
       <h1 >Web Tasarimi ve Programlama </h1 >
       <h2 >LAB -1 </h2 >
       <p> Ad Soyad : [ Sıla Kozik  ]</p>
+      <p> Bölüm : [ Yazılım Mühendsiliği  ]</p>
       <p> Ogrenci No: [ 235541039 ]</p>
     </div >
   )
